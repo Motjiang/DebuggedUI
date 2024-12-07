3804718bc4a98deee38b9344580a1008e6130472
Motjiang <motjiangbrilliant@gmail.com>
2024-12-07 20:21:32 +0200

    Added providers: HttpIntercepters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './authorization/login/login.component';
 
-import { HttpClientModule} from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignupComponent } from './authorization/signup/signup.component';
 import { ProfileComponent } from './authorization/profile/profile.component';
+import { InterceptorService } from './authorization/services/interceptor.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,13 @@ import { ProfileComponent } from './authorization/profile/profile.component';
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS, 
+      useClass:InterceptorService,
+      multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
