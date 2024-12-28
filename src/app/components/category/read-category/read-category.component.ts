ac2ef5f8a82c3799d275c9272ba20cb266321b25
Motjiang <motjiangbrilliant@gmail.com>
2024-12-29 00:24:42 +0200

    Added the get all categories method

--- src/app/components/category/read-category/read-category.component.ts
+++ src/app/components/category/read-category/read-category.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-read-category',
   templateUrl: './read-category.component.html',
   styleUrls: ['./read-category.component.css']
 })
-export class ReadCategoryComponent {
+export class ReadCategoryComponent implements OnInit {
 
+  categories$? : Observable<Category[]>
+
+
+  constructor(private categoryService: CategoryService){
+
+  }
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+  }
 }
---
+++ src/app/components/category/services/category.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllCategories():Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.baseUrl}categories`);
+  } 
+}
