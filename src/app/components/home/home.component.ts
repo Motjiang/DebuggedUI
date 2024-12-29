091b958e26574508cae5d2f6c495458d48850381
Motjiang <motjiangbrilliant@gmail.com>
2024-12-29 04:47:08 +0200

    Added a home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../article/models/article';
+import { Observable } from 'rxjs';
+import { ArticleService } from '../article/services/article.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  article$?: Observable<Article[]>;
+  constructor(private articleService: ArticleService) {
+
+  }
+  ngOnInit(): void {
+    this.article$ = this.articleService.getAllArticles();
+  }
 }
