e09790838f0ee0bb5fb0f70b07e9b1e2f0c0b1c7
Motjiang <motjiangbrilliant@gmail.com>
2024-12-30 05:50:57 +0200

    Added 2 main home articles

--- src/app/components/home-components/main-home-article/main-home-article.component.ts
+++ src/app/components/home-components/main-home-article/main-home-article.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Article } from '../../article/models/article';
+import { ArticleService } from '../../article/services/article.service';
 
 @Component({
   selector: 'app-main-home-article',
   templateUrl: './main-home-article.component.html',
   styleUrls: ['./main-home-article.component.css']
 })
-export class MainHomeArticleComponent {
+export class MainHomeArticleComponent implements OnInit {
+
+  articles$?: Observable<Article[]>;
+
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.articles$ = this.articleService.getAllArticles().pipe(
+      map(articles => articles.slice(0, 2)) // Take only the first two articles
+    );
+  }
 
 }
